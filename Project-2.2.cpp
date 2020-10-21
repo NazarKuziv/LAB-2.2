d5cb2adf4db8267d5665dec70ebcf1798422aca1
Nazar Kuziv <nazar123kuziv@gmail.com>
2020-10-21 15:26:23 +0300

    second commit

--- Project-2.2.cpp
+++ Project-2.2.cpp
@@ -13,16 +13,16 @@ int main()
     double Pi = 4 * atan(1.); // число пі
     double a;   // вхідний параметр
     double z1;  // результат 1 виразу
-  //  double z2;  // результат 2 виразу
+    double z2;  // результат 2 виразу
 
     cout << "a = "; cin >> a;
 
     z1 = cos(a) + sin(a) + cos(3 * a) + sin(3 * a);
-  //  z2 = 2 * sqrt(2) * cos(a) * sin((Pi / 4) + 2 * a);
+    z2 = 2 * sqrt(2) * cos(a) * sin((Pi / 4) + 2 * a);
 
     cout << endl;
     cout << "z1 = " << z1 << endl;
-  //  cout << "z2 = " << z2 << endl;
+    cout << "z2 = " << z2 << endl;
 
     cin.get();
     return 0;
